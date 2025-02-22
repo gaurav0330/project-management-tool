c11e22f3a4896fefac3b95d6f6f98a75e79503dc
gaurav0330 <gauravjikar070806@gmail.com>
2025-02-22 16:38:52 +0530

    tailwind setup by gaurav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import './App.css';
-import SideBar from './components/Other/sideBar'; // Capitalized component name
 import Card from './components/Other/card';
 
 function App() {
   return (
     <>
-      <SideBar /> {/* Capitalized component usage */}
       <Card title="Card Title" content="Card Content" />
     </>
   );
