5e4b5dffb31bf4070b10481bc41f3c93453c4729
gaurav0330 <gauravjikar070806@gmail.com>
2025-02-23 15:11:29 +0530

    ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,18 @@ import './App.css';
 import './index.css';
 import Dashboard from './pages/Home/Dashboard';
 import AdminDashboard from './pages/Admin/AdminDashboard';
-
+import Navbar from './components/Other/NavBar';
+import Project from './components/Other/card';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="main-content">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/admin" element={<AdminDashboard />} />
-          
+          {/* Add more routes as needed */}
         </Routes>
       </div>
     </Router>
