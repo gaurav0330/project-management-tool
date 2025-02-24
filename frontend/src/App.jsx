95572a6ccc3b7885cd6dfee771769b9ec3053d1d
gaurav0330 <gauravjikar070806@gmail.com>
2025-02-24 16:58:05 +0530

    Merge branch 'master' of https://github.com/gaurav0330/project-management-tool into gaurav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import './index.css';
 import Dashboard from './pages/Home/Dashboard';
 import AdminDashboard from './pages/ProjectManager/AdminDashboard';
 import ProjectDashboard from './pages/ProjectManager/ProjectDashboard';
 import AssignMembers from './pages/ProjectManager/AssignMembers';
 
-
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-<<<<<<< HEAD
-    {/* <Navbar />
-      <SideBar /> {/* Capitalized component usage */}
-      {/* <Card title="Card Title" content="Card Content" />
-      <Loader /> */} 
-      <Dashboard />
-=======
-      <Card title="Card Title" content="Card Content" />
->>>>>>> origin/master
-    </>
-=======
     <Router>
       <div className="main-content">
         <Routes>
@@ -33,8 +18,6 @@ function App() {
         </Routes>
       </div>
     </Router>
- 
->>>>>>> b22c77d7b76efcaef56e8f216ad3597238e59c66
   );
 }
 
