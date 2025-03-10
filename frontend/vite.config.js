d791de6498cd87603b01fd0dd52e4f39253fb398
gaurav0330 <gauravjikar070806@gmail.com>
2025-03-10 10:36:29 +0530

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/graphql": {
-        target: "https://backend-proxy.gauravjikar070806.workers.dev",
-        changeOrigin: true,
-        secure: true,
-      },
-      "/ws": {
-        target: "https://backend-proxy.gauravjikar070806.workers.dev",
-        changeOrigin: true,
-        ws: true,
-      },
-    },
-  },
-});
+})
